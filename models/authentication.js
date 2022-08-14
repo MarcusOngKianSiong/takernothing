d1a9c45aa5a5ba3d200a084da721226eb92c39b9
marcus <marcusongkiansiong@gmail.com>
2022-08-14 16:24:58 +0800

    authentication

--- models/authentication.js
+++ models/authentication.js
@@ -5,5 +5,7 @@ const authentication = express.Router();
 authentication.get("/login",(req,res)=>{
     const username = req.username;
     const password = req.password;
-    
+    console.log(username,password)
 });
+
+module.exports = authentication
