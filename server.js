41c989e8a489d5befa5b73540ee746fb45db2fcc
Marcus <marcusongkiansiong@gmail.com>
2022-08-18 11:04:37 +0800

    cleaning up code after creation of the basic functionalities

--- server.js
+++ server.js
@@ -108,61 +108,7 @@ io.on("connection",(socket)=>{
         console.log(data)
         await notesData.findOneAndUpdate({username: data.username,identificationNumber: data.identificationNumber}, {contents: data.contents});
     })
-
-
-
-    socket.on("editing",async (items)=>{
-        console.log("--------------------")
-        console.log("username: ",items.username);
-        console.log("identificationNumber: ",items.identificationNumber);
-        console.log("content: ",items.contents);
-        await notesData.findOneAndUpdate({username: items.username, identificationNumber: items.identificationNumber},{contents: items.contents});
-        //await was the problem here. right now this works because i'm going to assume you seed the data then hardcode username. However, your schema do not match at all. That will cause a lot of problems later.
-        // notesData.updateOne({username: "marcus"},{content: "hello?"});
-    })
-
-    socket.on("addNewNote",async (username)=>{
-        console.log("ENTERED!!!")
-        let newNumberOfNotes = 0;
-        notesData.find({username: username},(err,data)=>{
-            console.log("ERROR!!! --> ",username)
-            console.log({username: username, identificationNumber: newNumberOfNotes, contents: "Write something!!!"})
-            const user = username;
-            newNumberOfNotes = data.length+1;
-            notesData.create({identificationNumber: newNumberOfNotes, username: user, contents: "Write something!!!"});
-            socket.emit("Refresh note list",newNumberOfNotes);
-        })
-    })
-
-    socket.on("changeContent",(notesMetaData)=>{
-        notesData.find({username: notesMetaData.username,identificationNumber: notesMetaData.identificationNumber},(err,data)=>{
-            const contents = data[0].contents;
-            socket.emit("changeContent",contents);
-        })
-        // notesData.find({username: usernameAndIdentificationNumber.username, identificationNumber: usernameAndIdentificationNumber.identificationNumber},(data)=>{
-        //     console.log(data)
-        //     // socket.emit("changeContent",data.contents);
-        // });
-    })
-    
-    socket.on("deleteNote",(metaData)=>{
-        console.log("deleting data: ",metaData);
-        notesData.deleteOne(metaData, async (err)=>{
-            const deletedIdentificationNumber = metaData.identificationNumber;
-            const user = metaData.username;
-            notesData.find({username: user},(err,data)=>{ 
-                // Any identification number above deletedIdentificationNumber should be reduced by 1
-                data.forEach((note)=>{
-                    if(note.identificationNumber > deletedIdentificationNumber){
-                        notesData.updateOne({username: note.username, identificationNumber: note.identificationNumber},{identificationNumber: note.identificationNumber-1})
-                    }
-                }) 
-                notesData.find({username: user},(err,data)=>{
-                    socket.emit("Refresh note list after delete",data);
-                })
-            })
-        });
-    }) 
+ 
 
 })
 
